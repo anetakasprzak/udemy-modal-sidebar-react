7f94834de5f20526c4849bf52aedc1654c2c483b
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-29 18:05:23 +0000

    home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,18 @@
 import { useGlobalContext } from "./context";
+import { FaBars } from "react-icons/fa";
 
 const Home = () => {
   const { openSidebar, openModal } = useGlobalContext();
-  return <div>Home</div>;
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
 };
 
 export default Home;
